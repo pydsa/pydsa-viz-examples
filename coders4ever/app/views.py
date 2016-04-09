e3571b95de5a9df0cc6fdb5112f61a4741cf8787
Rehas Sachdeva <aquannie@gmail.com>
2016-04-10 03:10:44 +0530

    Delete unnecessary tutorial code

--- coders4ever/app/views.py
+++ coders4ever/app/views.py
@@ -4,21 +4,4 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = defineUser()
-    posts = [  # fake array of posts
-        { 
-            'author': {'nickname': 'John'}, 
-            'body': 'Beautiful day in Portland!' 
-        },
-        { 
-            'author': {'nickname': 'Susan'}, 
-            'body': 'The Avengers movie was so cool!' 
-        }
-    ]
-    return render_template("index.html",
-                           title='Home',
-                           user=user,
-                           posts=posts)
-
-def defineUser():
-	return {'nickname': 'Miguel'} 
+    return render_template("index.html")
